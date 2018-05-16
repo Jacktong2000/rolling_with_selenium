a01b013a55d51d70112fa1fdef9fcba358c1d946
jacktong2000 <jacktong2000@gmail.com>
2018-05-16 13:30:42 -0400

    first commit

--- selen_scrape.py
+++ selen_scrape.py
@@ -10,20 +10,46 @@ from pyvirtualdisplay import Display
 driver = webdriver.Chrome("/mnt/c/Users/TheOn/AppData/Local/Programs/Python/Python36-32/selenium/webdriver/chromedriver_win32/chromedriver.exe")
 
 #site to open
-driver.get('http://google.com');
 
-#time.sleep(2);
+def google():
+    driver.get('http://google.com');
 
-search_box= driver.find_element_by_name('q')
-search_box.send_keys('John Oliver')
-search_box.submit()
+    #time.sleep(2);
 
-#Finds link on Google search results
-search_result = driver.find_elements_by_class_name('r');
-for i in search_result:
-    print(i.find_element_by_css_selector('a').get_attribute('href'))
+    search_box= driver.find_element_by_name('q')
+    search_box.send_keys('John Oliver')
+    search_box.submit()
 
+    #Finds link on Google search results
+    links = []
+    search_result = driver.find_elements_by_class_name('r');
+    for i in search_result:
+        #print(i.find_element_by_css_selector('a').get_attribute('href'))
+        links.append(i.find_element_by_css_selector('a'))
 
-time.sleep(2)
+    links[4].click()
+    time.sleep(2)
 
-driver.quit()
+    driver.quit()
+
+google()
+
+def cnn_img():
+    driver.get('http://cnn.com');
+
+    search_result = driver.find_elements_by_css_selector('img')
+    for i in search_result:
+        print (i.get_attribute('src'))
+    driver.quit()
+
+def cnn_headline(keyword):
+    driver.get('http://cnn.com');
+
+    search_result = driver.find_elements_by_class_name('cd__headline-text')
+    for i in search_result:
+        if keyword.lower() in i.text.lower():
+            print(i.text)
+    #print (search_result.text)
+    driver.quit()
+
+#cnn_headline(input('Enter keyword: '))
